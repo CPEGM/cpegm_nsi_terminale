a2b0c519abbb71a0bb021f21a28cf19ab180c08f
Kaman <Kaman16384@users.noreply.github.com>
2022-01-27 10:36:29 +0100

    Correction sujet 27 terminée

--- tp/2022-correction/sujet-27.py
+++ tp/2022-correction/sujet-27.py
@@ -11,27 +11,30 @@ Sujet n°27
 # Exercice 1
 
 
-def taille(d, c):
-    n = 0
-
-    return n
-
+def taille(arbre, lettre):
+    if arbre[lettre] == ['', '']:
+        return 1
+    else:
+        if arbre[lettre][0] == '':
+            return 1 + taille(arbre, arbre[lettre][1])
+        elif arbre[lettre][1] == '':
+            return 1 + taille(arbre, arbre[lettre][0])
+        else:
+            return 1 + taille(arbre, arbre[lettre][0]) + taille(arbre, arbre[lettre][1])
 
 a = {'F': ['B', 'G'], 'B': ['A', 'D'], 'A': ['', ''], 'D': ['C', 'E'],
      'C': ['', ''], 'E': ['', ''], 'G': ['', 'I'], 'I': ['', 'H'],
      'H': ['', '']}
 
-print(taille(a, 'F'))
-
 # Exercice 2
-
-
 def tri_iteratif(tab):
     for k in range(len(tab)-1, 0, -1):
-        imax = ...
-        for i in range(0, ...):
-            if tab[i] > ...:
+        imax = 0
+        for i in range(0, k):
+            if tab[i] > tab[imax]:
                 imax = i
-        if tab[imax] > ...:
-            ..., tab[imax] = tab[imax], ...
+        if tab[imax] > tab[k]:
+            tab[k], tab[imax] = tab[imax], tab[k]
     return tab
+
+assert  tri_iteratif([41, 55, 21, 18, 12, 6, 25]) == [6, 12, 18, 21, 25, 41, 55]
