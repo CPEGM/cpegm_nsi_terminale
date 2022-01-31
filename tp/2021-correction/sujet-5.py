ebe4e5603619399bc9fd10ba5918f7f79ff4a02e
Kaman <Kaman16384@users.noreply.github.com>
2022-01-31 09:52:20 +0100

    Fin du sujet n°5

--- tp/2021-correction/sujet-5.py
+++ tp/2021-correction/sujet-5.py
@@ -21,7 +21,7 @@ def tri_insertion(L):
     n = len(L)
 
     # cas du tableau vide
-    if tab == []:
+    if L == []:
         return L
 
     for j in range(1,n):
@@ -31,13 +31,13 @@ def tri_insertion(L):
     # A l'étape j, le sous-tableau L[0,j-1] est trié
     # et on insère L[j] dans ce sous-tableau en déterminant
     # le plus petit i tel que 0 <= i <= j et L[i-1] > L[j].
-        while  i > 0 and L[i-1] > ...:
-            i = ...
+        while  i > 0 and L[i-1] > L[j]:
+            i = i-1
 
         # si i != j, on décale le sous tableau L[i,j-1] d’un cran
         # vers la droite et on place L[j] en position i
         if i != j:
-            for k in range(j,i,...):
-                L[k] = L[...]
-            L[i] = ...
+            for k in range(j,i,-1):
+                L[k] = L[k-1]
+            L[i] = e
     return L
